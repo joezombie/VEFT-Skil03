11eae4dbff0e303a9ea8bb033d73fc15f4db2aaf
Johannes Gunnar Heidarsson <joezombie@gmail.com>
2014-10-30 16:54:28 +0000

    Parse JSON

--- kodemon-server/kodemon.js
+++ kodemon-server/kodemon.js
@@ -3,13 +3,16 @@ var dgram = require("dgram");
 var server = dgram.createSocket("udp4");
 
 server.on("message", function(msg, rinfo){
-  console.log('got message from client: ' + msg);
+	data = JSON.parse(msg);
+	data.timestamp = new Date(data.timestamp * 1000);
+  	console.log('got message from client: ');
+  	console.log(data);
 });
 
 server.on('listening', function(){
-  console.log('Kodemon server listening on')
-  console.log('hostname: ' + server.address().address);
-  console.log('port: ' + server.address().port);
+  	console.log('Kodemon server listening on')
+  	console.log('hostname: ' + server.address().address);
+  	console.log('port: ' + server.address().port);
 });
 
 server.bind(4000)
