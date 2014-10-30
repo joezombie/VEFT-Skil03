421e4565d6a070b09b75beb8d615d81a363dfae4
Johannes Gunnar Heidarsson <joezombie@gmail.com>
2014-10-30 17:41:57 +0000

    Saving to MongoDB

--- kodemon-server/kodemon.js
+++ kodemon-server/kodemon.js
@@ -1,4 +1,7 @@
-var dgram = require("dgram");
+var dgram = require("dgram"),
+	mongoose = require('mongoose'),
+	Message = require('./models').Message;
+
 
 var server = dgram.createSocket("udp4");
 
@@ -6,7 +9,14 @@ server.on("message", function(msg, rinfo){
 	data = JSON.parse(msg);
 	data.timestamp = new Date(data.timestamp * 1000);
   	console.log('got message from client: ');
-  	console.log(data);
+  	var message = new Message(data);
+  	message.save(function(err, b){
+  		if(err){
+  			console.log('Could not save to MongoDB', err);
+  		} else {
+			console.log('Message saved to MongoDB');
+  		}
+  	});
 });
 
 server.on('listening', function(){
@@ -14,5 +24,14 @@ server.on('listening', function(){
   	console.log('hostname: ' + server.address().address);
   	console.log('port: ' + server.address().port);
 });
+
+function connectMongo(){ 
+  mongoose.connect('mongodb://localhost/kodemon', {keepAlive: 1});
+  console.log('Connecting to mongodb');
+};
+
+mongoose.connection.on('disconnected', connectMongo);
+
+connectMongo();
 
 server.bind(4000)
---
+++ kodemon-server/models.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+
+
+var setTags = function(tags){
+  return tags.split(',') 
+}
+
+var getTags = function(tags){
+  return tags.join(',')
+}
+
+var messageSchema = new mongoose.Schema({
+  execution_time:  {type: Number, required: true},
+  timestamp: {type: Date, required: true},
+  token: {type: String, required: true},
+  key: {type: String, required: true},
+});
+
+messageSchema.pre('save', function(next){
+  next();
+});
+
+messageSchema.post('save', function(b) {
+
+});
+
+
+var Message = mongoose.model('Message', messageSchema);
+
+module.exports = {'Message': Message};
